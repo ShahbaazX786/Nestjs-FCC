ee7a4bd1ddf4451b275bba8ec4724bf1fdd532d2
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-01 22:59:39 +0530

    Added controllers , services and how they work.

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable({})
+export class AuthService {
+  signup() {
+    return 'Bismillah';
+  }
+
+  signin() {
+    return 'Alhamdullilah';
+  }
+}
